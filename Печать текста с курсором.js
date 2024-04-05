22d0890b0bfa7d45947bed35501290c672e633a2
NikitinIlya0204 <nikitintube@gmail.com>
2024-04-05 10:49:56 +0300

    Задержка печати
    Добавил возможность определять в какой момент начнется печать

--- Печать текста с курсором.js
+++ Печать текста с курсором.js
@@ -20,11 +20,14 @@ if (textLength < sourceText.length || showCursor) {
   sourceText.substring(0, textLength);
 }
 
-// Вариант кода, чтобы курсор пропадал после того, как текст напечатан
+// Вариант кода, чтобы курсор пропадал после того, как текст напечатан\
+// Также теперь можно определять с какого момента начнется печать
 
 var cursor = "|"; // Символ курсора
 var fps = 10; // Скорость "печатания", символов в секунду
-var currentTime = time - thisLayer.inPoint;
+var startDelay = 0.5; // Задержка перед началом печати текста в секундах
+var currentTime = time - thisLayer.inPoint - startDelay; // Вычитаем задержку из текущего времени
+currentTime = Math.max(currentTime, 0); // Гарантируем, что время не будет отрицательным
 var sourceText = thisLayer.text.sourceText.value;
 var textLength = Math.min(Math.floor(currentTime * fps), sourceText.length);
 var blinkingSpeed = 0.5; // Скорость мигания курсора в секундах
