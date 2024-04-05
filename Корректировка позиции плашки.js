2306dfe655cfe03cb203235882bc809df762b286
NikitinIlya0204 <nikitintube@gmail.com>
2024-04-05 12:02:49 +0300

    Update Корректировка позиции плашки.js

--- Корректировка позиции плашки.js
+++ Корректировка позиции плашки.js
@@ -1,5 +1,8 @@
-// Центрирование плашки по левому краю 
 // Вставлять в Position шейпа
+// При использовании этого кода, плашка смещается, верните ее назад
+// с помощью параметра Anchor point
+
+# Центрирование плашки по левому краю 
 var originalSize = [100, 100]; // Начальный размер вашего Shape Layer
 var originalPosition = [960, 540]; // Начальная позиция вашего Shape Layer
 
@@ -12,3 +15,20 @@ var deltaX = (newSize[0] - originalSize[0]) / 2;
 
 // Корректируем позицию
 [originalPosition[0] + deltaX, value[1]];
+
+ # Центрирование плашки по левому и верхнему краю
+// Плашка будет расширяться только вправо и вниз
+
+var originalSize = [100, 100]; // Начальный размер вашего Shape Layer
+var originalPosition = [960, 540]; // Начальная позиция вашего Shape Layer
+
+// Получаем текущий размер прямоугольника
+var rectPath = content("Rectangle 1").content("Rectangle Path 1");
+var newSize = rectPath.size;
+
+// Рассчитываем изменение размера
+var deltaX = (newSize[0] - originalSize[0]) / 2;
+var deltaY = (newSize[1] - originalSize[1]) / 2;
+
+// Корректируем позицию
+[originalPosition[0] + deltaX, originalPosition[1] + deltaY];
