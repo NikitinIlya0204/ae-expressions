291b9314a56254b6fd648e586ecfd6f433419103
NikitinIlya0204 <nikitintube@gmail.com>
2024-04-08 14:25:05 +0300

    Update Корректировка позиции плашки.js

--- Корректировка позиции плашки.js
+++ Корректировка позиции плашки.js
@@ -36,3 +36,24 @@ var deltaY = (newSize[1] - originalSize[1]) / 2;
 
 // Корректируем позицию
 [originalPosition[0] + deltaX, originalPosition[1] + deltaY];
+
+
+//-------------------------------------------------------------------------
+// Центрирование плашки по левому и нижнему краю
+// Плашка будет расширяться только вправо и вверх
+//-------------------------------------------------------------------------
+
+
+var originalSize = [100, 100]; // Начальный размер вашего Shape Layer
+var originalPosition = [960, 540]; // Начальная позиция вашего Shape Layer
+
+// Получаем текущий размер прямоугольника
+var rectPath = content("Rectangle 1").content("Rectangle Path 1");
+var newSize = rectPath.size;
+
+// Рассчитываем изменение размера
+var deltaX = (newSize[0] - originalSize[0]) / 2;
+var deltaY = -(newSize[1] - originalSize[1]) / 2; // Инвертируем deltaY для расширения вверх
+
+// Корректируем позицию
+[originalPosition[0] + deltaX, originalPosition[1] + deltaY];
