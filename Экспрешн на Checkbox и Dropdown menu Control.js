50115b8106a4eb5e7ce5720e031efe8927a07a38
NikitinIlya0204 <nikitintube@gmail.com>
2024-07-22 12:29:02 +0300

    New

--- Экспрешн на Checkbox и Dropdown menu Control.js
+++ Экспрешн на Checkbox и Dropdown menu Control.js
@@ -1,6 +1,12 @@
+//Экспрешн на CheckBox
 // Используется в могрте для возможности включать и выключать
 //некоторые элементы плашки. Рекомендуется использовать с opacity
 //
 
 var checkbox = thisComp.layer("название слоя на котором Checkbox").effect("Название вашего Checkbox")("Checkbox").value;
 if (checkbox == 1){100} else {0};
+
+//Экспрешн на Dropdownmenu
+x = thisComp.layer("NULL ").effect("Dropdown Menu Control")("Menu");
+if (x==1) {100} // Вместо единицы порядковый номер нужного ответа
+else {0}
