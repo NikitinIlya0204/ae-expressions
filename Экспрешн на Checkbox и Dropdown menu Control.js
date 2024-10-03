6d888b081f026755d437a7af1b7e1a9d3dd1eb25
belyashik64 <165996273+belyashik64@users.noreply.github.com>
2024-10-03 09:44:13 +0300

    Update Экспрешн на Checkbox и Dropdown menu Control.js

--- Экспрешн на Checkbox и Dropdown menu Control.js
+++ Экспрешн на Checkbox и Dropdown menu Control.js
@@ -3,7 +3,7 @@
 //некоторые элементы плашки. Рекомендуется использовать с opacity
 //
 
-var checkbox = thisComp.layer("название слоя на котором Checkbox").effect("Название вашего Checkbox")("Checkbox").value;
+var checkbox = thisComp.layer("название слоя на котором Checkbox").effect("Название вашего Checkbox Control")("Checkbox").value;
 if (checkbox == 1){100} else {0};
 
 //Экспрешн на Dropdownmenu
